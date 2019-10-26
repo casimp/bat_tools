c0c489db41d7044b216c10eaa9c4f085777c5132
casimp <c.a.simpson01@gmail.com>
2019-10-26 19:46:42 +0100

    Added functionality to find all analysed wav files within folder and extract all their pulses. Also improved docstrings.

--- bat_tools/split_wav.py
+++ bat_tools/split_wav.py
@@ -7,9 +7,47 @@ Created on Fri Oct 18 13:28:08 2019
 import numpy as np
 import librosa
 import os
+import itertools
+
+species_list = ['PIPI', 'NYNO', 'MYO', 'RHHI', 'EPSE', 'PYPI', 'BABA', 'PLAU',
+                'NYLE', 'RHFE', 'PINA'] ## add the rest
+
 
 def load_split_wav(f, n_fft=2048, n_mels=256, hop_length=None, fmin=10000, fmax=150000):
+    """
+    Loads wav file, converts to mel spectogram that has been split into short 
+    (0.5s) sections for the purpose of markinng the pulse locations.
     
+    Parameters
+    ----------
+    f: str
+        The path to the wav file
+    n_fft: int
+        Length of FFT window
+    n_mels: int
+        Number of bins for the frequency data (using a mel filter bank 
+        construction)
+    hop_length: int/None
+        Number of samples between successive frames.
+    fmin: int
+        Minimum frequency for mel bins
+    fmax: int
+        Maximum frequency for mel bins
+    
+
+    Returns
+    -------
+    rate: int
+        Sampling rate
+    split: array
+        Array containing all n logmel spectograms for all pulses in specified 
+        file, of size (n , window, n_mels)
+    t: array
+        Time vector across each windows for each split
+    freq: array
+        Frequencies associated with the n_mel frequency bands
+
+    """
     # Set the default hop, if it's not already specified
     if hop_length is None:
         hop_length = int(n_fft // 4)
@@ -18,12 +56,6 @@ def load_split_wav(f, n_fft=2048, n_mels=256, hop_length=None, fmin=10000, fmax=
     melspec = librosa.feature.melspectrogram(sig, n_fft=2048, n_mels=n_mels, hop_length=hop_length, 
                                              fmin=fmin, fmax=fmax, sr=rate)
     logmel = librosa.core.power_to_db(melspec)
-    
-    
-    
-    # (sig, rate) = librosa.load(f, sr=None)
-    # S = np.abs(librosa.stft(sig, n_fft=n_fft, hop_length=hop_length,))
-    # log_S = librosa.amplitude_to_db(S, ref=np.max)
     t_total = sig.size/rate
     
     
@@ -31,14 +63,26 @@ def load_split_wav(f, n_fft=2048, n_mels=256, hop_length=None, fmin=10000, fmax=
     split = np.array_split(logmel, split_num, axis=1)
     t = np.linspace(0, (sig.size/rate), logmel.shape[1])
     t = np.array_split(t, split_num)
-    # f = np.linspace(0, rate/2000, logmel.shape[0])
     
     f = librosa.core.mel_frequencies(n_mels=n_mels, fmin=fmin, fmax=fmax)
     return rate, split, t, f
     
 def closest_argmin(A, B):
     """
-    Find A within B
+    For each element in A finds the index of the closest value in array B.
+    
+    Parameters
+    ----------
+    A: array
+        Array with elements to search for
+    B: array
+        Array to search within
+  
+
+    Returns
+    -------
+    rate: array
+        List of closest indices
     """
     L = B.size
     sidx_B = B.argsort()
@@ -47,12 +91,45 @@ def closest_argmin(A, B):
     sorted_idx[sorted_idx==L] = L-1
     mask = (sorted_idx > 0) & \
     ((np.abs(A - sorted_B[sorted_idx-1]) < np.abs(A - sorted_B[sorted_idx])) )
-    return sidx_B[sorted_idx-mask]
+    ind_list = sidx_B[sorted_idx-mask]
+    return ind_list
 
 
 def split_save(txt, window=256, n_fft=2048, n_mels=256, hop_length=None, fmin=10000, fmax=150000):
     """
-    Split at defined locations
+    Split wav at time points defined in associated txt file. Split data is
+    the melspectogram across the window of interest.
+    
+    Parameters
+    ----------
+    txt: str
+        The path to the folder structure to walk through
+    window: int
+        The number of points to take around the defined pulse (i.e time wrt 
+        sampling rate)
+    n_fft: int
+        Length of FFT window
+    n_mels: int
+        Number of bins for the frequency data (using a mel filter bank 
+        construction)
+    hop_length: int/None
+        Number of samples between successive frames.
+    fmin: int
+        Minimum frequency for mel bins
+    fmax: int
+        Maximum frequency for mel bins
+    
+
+    Returns
+    -------
+    split: array
+        Array containing all n logmel spectograms for all pulses in specified 
+        file, of size (n , window, n_mels)
+    t_split: array
+        Time vector across each windows for each pulse, with size (n, window)
+    freq: array
+        Frequencies associated with the n_mel frequency bands
+
     """
     # Set the default hop, if it's not already specified
     if hop_length is None:
@@ -87,4 +164,79 @@ def split_save(txt, window=256, n_fft=2048, n_mels=256, hop_length=None, fmin=10
     
     except (AssertionError, ValueError):
         pass
-#    
+
+def txt_list(path):
+    """
+    Walk through folder finding all analyzed files (i.e. wav files with associated
+    text files)
+    
+    Parameters
+    ----------
+    path: str
+        The path to the folder structure to walk through
+
+    Returns
+    -------
+    files: list
+        List of the absolute path to all the found text files (skips empty and
+        those labelled false i.e. incorrect labelling)
+    species_labels: list
+        List of species labels associated with those files
+    counts: list
+        Number of pulses within each file
+    min_space: list
+        The minimum spacing between pulses. For error checking (i.e. spacing
+        should be > 0)
+    """
+    files = []
+    counts = []
+    min_space = []
+    species_labels = []
+    # r=root, d=directories, f = files
+    for r, d, f in os.walk(path):
+        for file in f:
+            if '.txt' in file and os.path.splitext(file)[0].split('-')[-1].upper() in species_list:
+                fpath = os.path.join(r, file)
+                try:
+                    times = np.sort(np.loadtxt(fpath))
+                    
+                    min_space.append(np.min(times[1:] - times[:-1]))
+                    counts.append(times.size)
+                    files.append(fpath)
+                    species_labels.append(os.path.splitext(file)[0].split('-')[-1].upper())
+                except ValueError: # if False stored in file
+                    pass
+                
+    return files, species_labels, counts, min_space
+
+
+def split_save_path(path):
+    """
+    Walks through directory, finds all text files associated with pulse marked 
+    wav files, locates pulses and extracts logmel spectograms (256 x 256) for
+    each pulse. This window will cover > 1 pulse for most bat species.
+    
+    Parameters
+    ----------
+    path: str
+        The path to the folder structure to walk through
+
+    Returns
+    -------
+    d: array
+        Array containing all n logmel spectograms for all pulses across all 
+        files found in the folder structure of size (n , 256, 256)
+    sp_all: array
+        List of species associated with the dataset d, of size (n,)
+    """
+    files, species_labels, counts, min_space = txt_list(path)
+    data = []
+    sp_all = []
+    for f, sp in zip(files, species_labels):
+        X = split_save(f)
+        if X != None:
+            sp_all.append(X[0].shape[0] * [sp,])
+            data.append(X[0])
+    sp_all = np.array(list(itertools.chain(*sp_all)))
+    d = np.vstack(data)
+    return d, sp_all
